f8bb195d5f395a83dbf7aa9355b3bfaa6dae5db2
Mitutoyo <mitutoyum@proton.me>
2025-02-18 19:16:24 +0700

    added lock on

--- main.py
+++ main.py
@@ -1,64 +1,77 @@
-from pynput import keyboard
+# todo: add toggle key
+
+from pynput.keyboard import Controller, Listener
+from vgamepad import VX360Gamepad, XUSB_BUTTON
 from time import sleep
 
 
-def on_press(controller, toggles):
+class Macro:
+    toggle = True
+
+    lock_on = "c"
+    blackflash = False
+    earthquake = False
+
+
+def on_press(controller: Controller, gamepad: VX360Gamepad, macro: Macro):
     def callback(key):
-        if not isinstance(key, keyboard.KeyCode):
-            return
 
         keycode = str(key).strip("''")
 
-        if keycode not in ["3", "v"] or not key.vk:
+        if keycode not in ["3", "v", macro.lock_on] or not key.vk:
             return
 
         match keycode:
+
             case "3":
-                if toggles["bl"]:
+                if macro.blackflash:
                     return
 
-                toggles["bl"] = True
+                macro.blackflash = True
                 sleep(0.300)
                 controller.tap("3")
             case "v":
-                if toggles["eq"]:
+                if macro.earthquake:
                     return
 
-                toggles["eq"] = True
+                macro.earthquake = True
 
                 controller.press("3")
                 sleep(0.900)
                 controller.release("3")
 
+            case macro.lock_on:
+                gamepad.press_button(XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB)
+
+                gamepad.update()
+
+                gamepad.release_button(XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB)
+                gamepad.update()
+
     return callback
 
 
-def on_release(toggles):
+def on_release(macro: Macro):
     def callback(key):
-        if not isinstance(key, keyboard.KeyCode):
-            return
-
         keycode = str(key).strip("''")
 
         if keycode == "3":
-            if key.vk and toggles["bl"]:
-                toggles["bl"] = False
-            elif not key.vk and toggles["eq"]:
-                toggles["eq"] = False
+            if key.vk and macro.blackflash:
+                macro.blackflash = False
+            elif not key.vk and macro.earthquake:
+                macro.earthquake = False
 
     return callback
 
 
 def main() -> None:
-    controller = keyboard.Controller()
+    controller = Controller()
+    gamepad = VX360Gamepad()
+    macro = Macro()
 
-    toggles = {
-        "bl": False,  # blackflash
-        "eq": False,  # mahoraga's earthquake
-    }
-
-    with keyboard.Listener(
-        on_press=on_press(controller, toggles), on_release=on_release(toggles)
+    with Listener(
+        on_press=on_press(controller, gamepad, macro),
+        on_release=on_release(macro),
     ) as listener:
         listener.join()
 
